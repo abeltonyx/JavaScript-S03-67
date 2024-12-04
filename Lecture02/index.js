1dd98427f062a2378890cc76f13ff14ce3c60325
abeltonyx <thapanan.tpn@gmail.com>
2024-12-04 08:28:01 +0700

    secondcommit

---
+++ Lecture02/index.js
@@ -0,0 +1,12 @@
+const message = "Hello";
+const place = "World";
+
+// logging out a string
+console.log('Hell,Word');
+
+// using substitutions
+console.log('Hello,%s',place);
+
+// using a string literal 
+console.log( `${message},${place}! `)
+
---
+++ Lecture02/undefined.js
@@ -0,0 +1,9 @@
+function test(t){
+    if(t === undefined) {
+        return "Undefind value!";
+    }
+    return t;
+}
+
+let x;
+console.log(test(x));
